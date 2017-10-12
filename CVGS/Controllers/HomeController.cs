f788c62202acbebaf63488b1380faa2d8e740244
dougEpp <doug.epp@gmail.com>
2017-10-12 14:55:35 -0400

    Added login page

--- CVGS/Controllers/HomeController.cs
+++ CVGS/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CVGS.Models;
+using CVGS.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +11,8 @@ namespace CVGS.Controllers
 {
     public class HomeController : Controller
     {
+        private CVGSEntities db = new CVGSEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +30,21 @@ namespace CVGS.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login([Bind(Include = "UserName,Pwd")] LoginViewModel login)
+        {
+            //var memberId = db.SP_MEMBER_LOGIN(login.UserName, login.Pwd, sqlDBType);
+            ObjectParameter objectParameter = new ObjectParameter("memberId", typeof(int));
+            db.SP_MEMBER_LOGIN(login.UserName, login.Pwd, objectParameter);
+            var memberId = objectParameter.Value;
+            this.Session["MemberId"] = memberId;
+            return RedirectToAction("Index","Account");
         }
     }
 }
