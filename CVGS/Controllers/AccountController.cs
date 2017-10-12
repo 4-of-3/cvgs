4d51ff0c6e92f82af1f66d723f939f0168baf36b
dougEpp <doug.epp@gmail.com>
2017-10-12 14:54:53 -0400

    Added member and account management

---
+++ CVGS/Controllers/AccountController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CVGS.Models;
+
+namespace CVGS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private CVGSEntities db = new CVGSEntities();
+
+        // GET: MEMBERs
+        public ActionResult Index()
+        {
+            var memberId = this.Session["MemberId"];
+            if (memberId == null)
+            {
+                memberId = 3;
+            }
+            MEMBER mEMBER = db.MEMBERs.Find(memberId);
+            if (mEMBER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mEMBER);
+        }
+
+        // GET: MEMBERs/Create
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // POST: MEMBERs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult New([Bind(Include = "MemberId,FName,LName,UserName,Email,FavPlatform,FavCategory,FavGame,FavQuote,DateJoined,ActiveStatus,Pwd")] MEMBER mEMBER)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.SP_ADD_MEMBER(mEMBER.FName, mEMBER.LName, mEMBER.UserName, mEMBER.Email, mEMBER.Pwd, mEMBER.FavPlatform, mEMBER.FavCategory, mEMBER.FavGame, mEMBER.FavQuote);
+                db.MEMBERs.Add(mEMBER);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(mEMBER);
+        }
+
+        // GET: MEMBERs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MEMBER mEMBER = db.MEMBERs.Find(id);
+            if (mEMBER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mEMBER);
+        }
+
+        // POST: MEMBERs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MemberId,FName,LName,UserName,Email,FavPlatform,FavCategory,FavGame,FavQuote,DateJoined,ActiveStatus,Pwd")] MEMBER mEMBER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mEMBER).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mEMBER);
+        }
+
+        // GET: MEMBERs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MEMBER mEMBER = db.MEMBERs.Find(id);
+            if (mEMBER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mEMBER);
+        }
+
+        // POST: MEMBERs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MEMBER mEMBER = db.MEMBERs.Find(id);
+            db.MEMBERs.Remove(mEMBER);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CVGS/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CVGS.ViewModels
+{
+    public class LoginViewModel
+    {
+        private string userName;
+        private string pwd;
+
+        public string Pwd { get => pwd; set => pwd = value; }
+        public string UserName { get => userName; set => userName = value; }
+    }
+}
