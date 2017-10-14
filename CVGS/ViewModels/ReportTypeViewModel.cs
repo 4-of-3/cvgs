709b070de943c9cef082ec7165a6a89a288df9b7
Kendall Roth <kroth@unavia.ca>
2017-10-13 20:54:17 -0400

    #51 Add basic reports page

---
+++ CVGS/ViewModels/ReportTypeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace CVGS.ViewModels
+{
+    public class ReportTypeViewModel
+    {
+        [DisplayName("Report Type")]
+        public string ReportType { get; set; }
+
+        [DisplayName("Enabled")]
+        public bool Enabled { get; set; }
+
+        public string ReportURL { get; set; }
+    }
+}
