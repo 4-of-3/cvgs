25595cb5880a85c9e8cc683d3aa27ef2993671c3
Kendall Roth <kroth@unavia.ca>
2017-10-13 20:14:16 -0400

    48 update annotations (#50)
    * #37 Update password for Kendall
    
    * #37 Remove unnecessary header links
    
    * #37 Fix invalid Logout link
    
    * #48 Add partial Game class for annotations and update views
    
    * #48 Update login/create account pages
    
    * #48 Minor updates to Games pages
    
    * #48 Minor UI tweaks for Editing Events
    
    * #48 Use proper HTML helpers (with updated meta models)
    
    * #48 Refactor Event sorting logic
    
    * #48 Add annotations to Member class

---
+++ CVGS/Models/EventMeta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CVGS.Models
+{
+    [MetadataType(typeof(EventMeta))]
+    public partial class EVENT
+    {
+    }
+
+    public class EventMeta
+    {
+        [DisplayName("ID")]
+        public int EventId { get; set; }
+
+        [DisplayName("Title")]
+        [Required]
+        public string EventTitle { get; set; }
+
+        [DisplayName("Description")]
+        [Required, MaxLength(1024)]
+        public string Description { get; set; }
+
+        [DisplayName("Date")]
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        public System.DateTime EventDate { get; set; }
+
+        [DisplayName("Active")]
+        public bool ActiveStatus { get; set; }
+
+        [DisplayName("Date Created")]
+        public Nullable<System.DateTime> DateCreated { get; set; }
+
+        [DisplayName("Location")]
+        [Required, MaxLength(64)]
+        public string Location { get; set; }
+    }
+}
---
+++ CVGS/Models/GameMeta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CVGS.Models
+{
+    [MetadataType(typeof(GameMeta))]
+    public partial class GAME
+    {
+    }
+ 
+    public class GameMeta
+    {
+        [DisplayName("ID")]
+        public int GameId { get; set; }
+
+        [DisplayName("Title")]
+        [Required]
+        public string Title { get; set; }
+
+        [DisplayName("ISBN")]
+        [Required, MaxLength(10)]
+        public string ISBN { get; set; }
+
+        [DisplayName("Developer")]
+        [Required]
+        public string Developer { get; set; }
+
+        [DisplayName("Description")]
+        [Required, MaxLength(1024)]
+        public string Description { get; set; }
+
+        [DisplayName("Category")]
+        [Required]
+        public string Category { get; set; }
+
+        [DisplayName("Published")]
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime PublicationDate { get; set; }
+
+        [DisplayName("Cost")]
+        [Required, DataType(DataType.Currency)]
+        public decimal Cost { get; set; }
+
+        [DisplayName("Digital")]
+        [Required]
+        public bool Digital { get; set; }
+    }
+}
