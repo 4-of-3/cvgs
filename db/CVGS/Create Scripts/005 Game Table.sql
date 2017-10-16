337b5e261be3b987b5d48e2ff38517cdaf11c67a
dougEpp <doug.epp@gmail.com>
2017-10-16 13:45:24 -0400

    Replaced tabs with spaces in sql files

--- db/CVGS/Create Scripts/005 Game Table.sql
+++ db/CVGS/Create Scripts/005 Game Table.sql
@@ -1,17 +1,17 @@
 IF EXISTS(SELECT TABLE_NAME from INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'GAME')
-	DROP TABLE GAME;
+        DROP TABLE GAME;
 GO
 
 CREATE TABLE CVGS.dbo.GAME(
-	  GameId 		    INT IDENTITY( 1,1 ) PRIMARY KEY
-	, Title 		    NVARCHAR(   64 ) NOT NULL
-	, ISBN 			    NVARCHAR(   10 ) NOT NULL
-	, Developer 	    NVARCHAR(   64 ) NOT NULL
-	, Description 	    NVARCHAR( 1024 ) NOT NULL
-	, Category 		    NVARCHAR(   64 ) NOT NULL
-	, PublicationDate 	Date NOT NULL
-	, Cost 	 		    MONEY NOT NULL
-	, Digital			BIT NOT NULL DEFAULT 0
-	, Deleted           BIT NOT NULL DEFAULT 0
-	);
+          GameId                     INT IDENTITY( 1,1 ) PRIMARY KEY
+        , Title                     NVARCHAR(   64 ) NOT NULL
+        , ISBN                             NVARCHAR(   10 ) NOT NULL
+        , Developer             NVARCHAR(   64 ) NOT NULL
+        , Description             NVARCHAR( 1024 ) NOT NULL
+        , Category                     NVARCHAR(   64 ) NOT NULL
+        , PublicationDate         Date NOT NULL
+        , Cost                              MONEY NOT NULL
+        , Digital                        BIT NOT NULL DEFAULT 0
+        , Deleted           BIT NOT NULL DEFAULT 0
+        );
 GO
--- db/CVGS/Create Scripts/011 Game_Platform Table.sql
+++ db/CVGS/Create Scripts/011 Game_Platform Table.sql
@@ -1,10 +1,10 @@
 IF EXISTS(SELECT TABLE_NAME from INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'GAME_PLATFORM')
-	DROP TABLE GAME_PLATFORM;
+        DROP TABLE GAME_PLATFORM;
 GO
 
 CREATE TABLE CVGS.dbo.GAME_PLATFORM(
-		   GameId        INT NOT NULL
-		 , PlatformId         INT NOT NULL
+                   GameId        INT NOT NULL
+                 , PlatformId         INT NOT NULL
 CONSTRAINT pk_game_platform PRIMARY KEY( GameId, PlatformId),
 CONSTRAINT fk_game_platform FOREIGN KEY( GameId ) REFERENCES Game(GameId) ON DELETE CASCADE,
 CONSTRAINT fk_platform_game FOREIGN KEY( PlatformId ) REFERENCES Platform(PlatformId) ON DELETE CASCADE
