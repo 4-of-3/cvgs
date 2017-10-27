cd80161e83bb8e57a6b64b5d24059caed0c81eaa
dougEpp <doug.epp@gmail.com>
2017-10-27 11:03:47 -0400

    Added meta model for data annotations

---
+++ CVGS/Models/ReviewMeta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CVGS.Models
+{
+    [MetadataType(typeof(ReviewMeta))]
+    public partial class REVIEW
+    {
+    }
+    public class ReviewMeta
+    {
+        [Required]
+        public int MemberId { get; set; }
+        [Required]
+        public int GameId { get; set; }
+        [Display(Name = "Review")]
+        [StringLength(1024)]
+        public string ReviewText { get; set; }
+        [Display(Name = "Rating")]
+        [Required, Range(1,5)]
+        public int Rating { get; set; }
+        public System.DateTime DateCreated { get; set; }
+        public Nullable<System.DateTime> DateModified { get; set; }
+    }
+}
