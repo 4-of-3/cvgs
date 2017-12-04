9d94aa56c1ef5f8277c6b427d1094e0d2feb7b24
dougEpp <doug.epp@gmail.com>
2017-12-04 15:04:18 -0500

    #119 added quantity validation to cart items in cart view model

--- CVGS/ViewModels/CartViewModel.cs
+++ CVGS/ViewModels/CartViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CVGS.Models;
 using System;
+using System.Globalization;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +11,28 @@ namespace CVGS.ViewModels
     public class CartViewModel
     {
         public int MemberId { get; set; }
+        [Required]
+        [ItemQuantityMin(MinQuantity = 0,ErrorMessage = "Quantity cannot be less than 0")]
         public List<CARTITEM> CartItems { get; set; }
     }
+    sealed public class ItemQuantityMin : ValidationAttribute
+    {
+        public int MinQuantity;
+        public override bool IsValid(object items)
+        {
+            bool result = true;
+            List<CARTITEM> cartItems = (List<CARTITEM>) items;
+            foreach (var item in cartItems)
+            {
+                if (item.Quantity < MinQuantity)
+                {
+                    result = false;
+                }
+            }
+            // Add validation logic here.
+            return result;
+        }
+
+    }
+        
 }
