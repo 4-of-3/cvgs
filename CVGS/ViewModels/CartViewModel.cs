6edf6249b9996fb78c76dd128bd9a6d6b47b6602
Doug Epp <doug.epp@gmail.com>
2017-12-05 10:15:42 -0500

    #119 removed unused annotation on view model

--- CVGS/ViewModels/CartViewModel.cs
+++ CVGS/ViewModels/CartViewModel.cs
@@ -11,29 +11,6 @@ namespace CVGS.ViewModels
     public class CartViewModel
     {
         public int MemberId { get; set; }
-
-        [Required]
-        [ItemQuantityMin(MinQuantity = 0,ErrorMessage = "Quantity cannot be less than 0")]
         public List<CARTITEM> CartItems { get; set; }
     }
-    sealed public class ItemQuantityMin : ValidationAttribute
-    {
-        public int MinQuantity;
-        public override bool IsValid(object items)
-        {
-            bool result = true;
-            List<CARTITEM> cartItems = (List<CARTITEM>) items;
-            foreach (var item in cartItems)
-            {
-                if (item.Quantity < MinQuantity)
-                {
-                    result = false;
-                }
-            }
-            // Add validation logic here.
-            return result;
-        }
-
-    }
-        
 }
