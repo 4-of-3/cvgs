9638b09d0e1524a0d42fc43d218bb3de416e2f72
Lucas Benninger <benningerlucas@gmail.com>
2017-12-08 13:17:23 -0500

    Initial Employee Portal

---
+++ CVGS/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CVGS.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            // Redirect unauthenticated members
+            var memberId = this.Session["MemberId"];
+            if (memberId == null)
+            {
+                return RedirectToAction("Index", "Login"); ;
+            }
+
+            // Make Page Accessible to Employees and Admins only
+            if ((string)Session["MemberRole"] != "Employee" && (string)Session["MemberRole"] != "Admin")
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            }
+
+            return View();
+        }
+    }
+}
